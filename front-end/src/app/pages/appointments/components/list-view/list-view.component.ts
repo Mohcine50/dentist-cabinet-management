7e4e1da019d3db38a71b97b4c4fd5fa544ccd7eb
shegami <mohcinela1998@gmail.com>
2024-03-19 18:07:50 +0000

    setup list view

--- front-end/src/app/pages/appointments/components/list-view/list-view.component.ts
+++ front-end/src/app/pages/appointments/components/list-view/list-view.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { DataTableComponent } from '../../../../shared/components/dataTable/data-table.component';
 
 @Component({
   selector: 'dem-list-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, DataTableComponent],
   templateUrl: './list-view.component.html',
   styleUrl: './list-view.component.scss',
 })
-export class ListViewComponent {}
+export class ListViewComponent {
+  patientTableHeader: string[] = [
+    'Full Name',
+    'Treatment',
+    'Time & Date',
+    'Status',
+  ];
+
+  tableData = [
+    {
+      'Full Name': 'Mohcine Lahnin',
+      Treatment: 'Detartrage',
+      'Time & Date': '10:29 AM | 22/04/2024',
+      Status: 'Canceled',
+    },
+  ];
+}
