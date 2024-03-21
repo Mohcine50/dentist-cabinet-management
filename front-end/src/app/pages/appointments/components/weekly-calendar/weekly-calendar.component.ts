4df36775bf4eb454ce171e8a840b2f5eb40f4791
shegami <mohcinela1998@gmail.com>
2024-03-21 23:55:14 +0000

    complete change week days on the week calendar planner

--- front-end/src/app/pages/appointments/components/weekly-calendar/weekly-calendar.component.ts
+++ front-end/src/app/pages/appointments/components/weekly-calendar/weekly-calendar.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
+  add,
   eachDayOfInterval,
+  eachMinuteOfInterval,
   endOfMonth,
   endOfWeek,
+  format,
+  isSameMonth,
   isThisMonth,
   isToday,
+  isWithinInterval,
+  parse,
   startOfToday,
   startOfWeek,
+  sub,
 } from 'date-fns';
 
 @Component({
@@ -20,6 +27,15 @@ import {
 export class WeeklyCalendarComponent implements OnInit {
   todayDate = startOfToday();
   firstDayOfWeek = startOfWeek(this.todayDate, { weekStartsOn: 1 });
+  startHour = new Date().setHours(8, 30);
+  endHour = new Date().setHours(18);
+
+  minutes = eachMinuteOfInterval(
+    { start: this.startHour, end: this.endHour },
+    {
+      step: 15,
+    }
+  );
   protected readonly isThisMonth = isThisMonth;
   protected readonly isToday = isToday;
 
@@ -30,12 +46,51 @@ export class WeeklyCalendarComponent implements OnInit {
     });
   }
 
-  previousWeek() {}
-
-  nextWeek() {}
-
-  ngOnInit(): void {
-    console.log(this.firstDayOfWeek);
-    console.log(this.weekDays);
+  get filteredMinutes() {
+    return this.minutes.filter(
+      (minute) =>
+        !isWithinInterval(minute, {
+          start: new Date().setHours(11),
+          end: new Date().setHours(13),
+        })
+    );
   }
+
+  get currentMonth() {
+    const firstDay = this.weekDays[0];
+    const lastDay = this.weekDays[this.weekDays.length - 1];
+    if (isSameMonth(firstDay, lastDay)) return format(firstDay, 'LLLL yyyy');
+    else
+      return `${format(firstDay, 'LLLL')} to ${format(lastDay, 'LLLL yyyy')}`;
+  }
+
+  generateTimesFromDayTime(date: Date) {
+    const startTime = date.setHours(8, 30);
+    const endTime = date.setHours(18);
+    const minutes = eachMinuteOfInterval(
+      { start: startTime, end: endTime },
+      {
+        step: 15,
+      }
+    );
+    return minutes.filter(
+      (minute) =>
+        !isWithinInterval(minute, {
+          start: date.setHours(11),
+          end: date.setHours(13),
+        })
+    );
+  }
+
+  previousWeek() {
+    const previousWeek = sub(this.firstDayOfWeek, { weeks: 1 });
+    this.firstDayOfWeek = previousWeek;
+  }
+
+  nextWeek() {
+    const nextWeek = add(this.firstDayOfWeek, { weeks: 1 });
+    this.firstDayOfWeek = nextWeek;
+  }
+
+  ngOnInit(): void {}
 }
