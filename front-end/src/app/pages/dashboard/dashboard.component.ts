0f62c9eb77087c2ef7db5bae6bbe0dddd08c20ae
shegami <mohcinela1998@gmail.com>
2024-04-17 11:32:11 +0100

    add some items to the dashboard page

--- front-end/src/app/pages/dashboard/dashboard.component.ts
+++ front-end/src/app/pages/dashboard/dashboard.component.ts
@@ -2,12 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SideBarComponent } from 'src/app/shared/components/sideBar/sideBar.component';
-import { StatisticsCardComponent } from './components/statistics-card.component';
+import { StatisticsCardComponent } from './components/statistics-card/statistics-card.component';
+import { PatientsDemographicsComponent } from './components/patients-demographics/patients-demographics.component';
 
 @Component({
   selector: 'dem-dashboard',
   standalone: true,
-  imports: [CommonModule, SideBarComponent, RouterModule, StatisticsCardComponent],
+  imports: [
+    CommonModule,
+    SideBarComponent,
+    RouterModule,
+    StatisticsCardComponent,
+    PatientsDemographicsComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
