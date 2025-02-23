7a85f4bfeb458835fe6e0291b575fa485f07e525
Mohcine lahnin <mohcinela1998@gmail.com>
2025-02-23 18:27:27 +0000

    adjust staff card

--- frontend/src/app/pages/staff/staff-card/staff-card.component.ts
+++ frontend/src/app/pages/staff/staff-card/staff-card.component.ts
@@ -1,20 +1,27 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { staff } from '../models/staff/staff.model';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { staff } from "../models/staff/staff.model";
 
 @Component({
-  selector: 'dem-staff-card',
+  selector: "dem-staff-card",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './staff-card.component.html',
-  styleUrl: './staff-card.component.scss',
+  templateUrl: "./staff-card.component.html",
+  styleUrl: "./staff-card.component.scss",
 })
 export class StaffCardComponent {
   @Input() staff!: staff;
+
+  @Output("show-details") show: EventEmitter<any> = new EventEmitter();
 
   showMenu = false;
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
+  }
+
+  showStaffDetails() {
+    this.show.emit();
+    this.toggleMenu();
   }
 }
